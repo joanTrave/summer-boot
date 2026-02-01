de7bc10d6aa0fec12d32924dcea3a53bbb95f8d5
Ton Llucia <tonlls1999@gmail.com>
2026-02-01 23:20:20 +0100

    test: add setup file for discovering services in IoC context tests.

--- tests/inversion_of_control/context/setup.py
+++ tests/inversion_of_control/context/setup.py
@@ -2,7 +2,7 @@ import os
 
 from summer.inversion_of_control.service_locator import discover_services
 
-executable_parent_path = os.getcwd()
+executable_parent_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def setup():
