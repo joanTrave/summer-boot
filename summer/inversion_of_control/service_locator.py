02d83b5b8baffcda1a3ae867bf448ce1db44a8cf
Ton Llucia <tonlls1999@gmail.com>
2026-02-01 23:13:42 +0100

    fix testing

--- summer/inversion_of_control/service_locator.py
+++ summer/inversion_of_control/service_locator.py
@@ -1,5 +1,6 @@
 import glob
 import importlib
+import importlib.util
 from pathlib import Path
 from typing import NoReturn
 
@@ -24,8 +25,13 @@ def discover_services(executable_parent_path: str) -> NoReturn:
     Discovers recursively all services that match with termination and execute them in order to save them to
     services dict.
     """
-    for filename in glob.iglob(executable_parent_path + f"{ALL_FILES_PATH}{PYTHON_EXTENSION}", recursive=True):
-        if f"{NEW_LINE_LITERAL}{SERVICE_DECORATOR_LITERAL}" in open(filename).read():
+    for filename in glob.iglob(
+        executable_parent_path + f"{ALL_FILES_PATH}{PYTHON_EXTENSION}", recursive=True
+    ):
+        if (
+            f"{NEW_LINE_LITERAL}{SERVICE_DECORATOR_LITERAL}"
+            in open(filename, encoding="utf-8").read()
+        ):
             module_name: str = get_module_name_from_absolute_path(filename)
             spec = importlib.util.spec_from_file_location(module_name, filename)
             foo = importlib.util.module_from_spec(spec)
--- tests/inversion_of_control/context/setup.py
+++ tests/inversion_of_control/context/setup.py
@@ -1,8 +1,8 @@
 import os
 
-from summer.inversion_of_control.service_locator import get_executable_parent_path, discover_services
+from summer.inversion_of_control.service_locator import discover_services
 
-executable_parent_path = get_executable_parent_path(os.getcwd())
+executable_parent_path = os.getcwd()
 
 
 def setup():
